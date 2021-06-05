5e897d531647e4990933159c202b42999256d1dd
M ILHAMSYAH <63888235+ilhamsyah183@users.noreply.github.com>
2021-06-06 04:33:52 +0700

    Add Key to buildConfig

--- core/src/main/java/com/dicoding/picodiploma/core/data/source/remote/network/ApiService.kt
+++ core/src/main/java/com/dicoding/picodiploma/core/data/source/remote/network/ApiService.kt
@@ -1,11 +1,11 @@
 package com.dicoding.picodiploma.core.data.source.remote.network
 
-import com.dicoding.picodiploma.core.BuildConfig
+import com.dicoding.picodiploma.core.BuildConfig.key
 import com.dicoding.picodiploma.core.data.source.remote.response.ListGameResponse
 import retrofit2.http.GET
 
 interface ApiService {
     //limit:20000 Request a month for this key
-    @GET("games?page=1&key=${BuildConfig.key}")
+    @GET("games?page=1&key=${key}")
     suspend fun getList(): ListGameResponse
 }
